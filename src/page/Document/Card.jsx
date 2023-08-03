f1a1a11187920bd1194fda710a18c6cf4e6ed10b
trong hieu <hieu745233@gmail.com>
2023-08-03 11:33:52 +0700

    Update

--- src/page/Document/Card.jsx
+++ src/page/Document/Card.jsx
@@ -12,7 +12,7 @@ function Card({title,des}) {
             <img src="https://i.imgur.com/3lNxiwz.jpg" alt="Ảnh"/>
             <Typography variant="h2" color="primary" className={cx("card-h2")}>{title}</Typography>
             <Typography variant="p"  className={cx("card-p")}>{des}</Typography>
-            <Link to='/document/ta' style={{textDecoration:'none'}}><Button variant="contained" color="primary">Xem tài liệu <ArrowCircleRightOutlined style={{marginLeft:'10px'}} /></Button></Link> 
+            <Link to='/document/ta' style={{textDecoration:'none'}}><Button variant="contained" color="primary">Xem tài liệu! <ArrowCircleRightOutlined style={{marginLeft:'10px'}} /></Button></Link> 
         </div>
       );
 }
